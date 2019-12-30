e30b1ae37a347f55e26a9535101fe7d2cc12715b
JeranSalcedo <janraymondsalcedo@gmail.com>
2019-12-30 20:27:22 +0800

    Alarm Test

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ client.on('message', message => {
 									if(channels[data.guildId] !== undefined && channels[data.guildId][0] !== undefined){
 										console.log(alarms[data.guildId][`${currentDate.getHours() + 9 > 23? String(currentDate.getHours() - 15).padStart(2, '0') : String(currentDate.getHours() + 9).padStart(2, '0')}${String(currentDate.getMinutes()).padStart(2, '0')}${String(currentDate.getSeconds()).padStart(2, '0')}`]);
 										message.guild.channels.get(channels[data.guildId][0])
-											.send('bleh');
+											.send('bleh')
 											.then(console.log(`Sent message: ${message.content}`))
 											.catch(console.error);
 									}
